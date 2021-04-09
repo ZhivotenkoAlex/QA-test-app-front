e1ae74b0db7a28b39f059f70fb5a2e49c4eb0b2c
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2021-04-09 16:23:25 +0300

    Added images, now the team members cards are displayed

--- src/views/ContactsPage/ContactsPage.js
+++ src/views/ContactsPage/ContactsPage.js
@@ -3,6 +3,14 @@ import s from './ContactsPage.module.css';
 import ContactCard from '../ContactCard/ContactCard'
 import imageAlex from "../../images/alex.jpg"
 import imageDasha from "../../images/daria.jpg"
+import imageTasia from "../../images/taisia.jpg"
+import imageOleksandr from "../../images/oleksandr.jpg"
+import imageOleksii from "../../images/oleksii.jpg"
+import imageLillia from "../../images/lilia.jpg"
+import imageIvan from '../../images/ivan.jpg'
+import imageFedir from "../../images/fedir.jpg"
+import imageAnton from "../../images/anton.jpg"
+import imageEkaterina from "../../images/ekaterina.jpg"
 
 
 function ContactsPage() {
@@ -12,13 +20,13 @@ function ContactsPage() {
         <>
             <section className={s.team}>
                 <h4 className={s.teamTitle}>Our team</h4>
-                <hr className="teamLine" />
+                <hr className={s.teamLine} />
 
                 <ul className={s.teamList}>
                     <li className={s.teamItem}>
                         <img src={imageAlex} alt="Alex" className={s.personPhoto} />
 
-                        <div className="aboutPerson">
+                        <div className={s.aboutPerson}>
                             <p className={s.personName}>Alex Zhyvotenko</p>
                             <p className={s.personPosition}>Front-End Developer</p>
                             <p className={s.personText}>Lorem Ipsum has been the standard "fish" for Latin texts since the early
@@ -30,83 +38,83 @@ function ContactsPage() {
 
 
 
-                    <li className="teamItem">
-                        <img src="./img/oleksii.jpg" alt="Olexii" className="personPhoto" />
-                        <div className="aboutPerson">
-                            <p className="personName">Olexii Fedorkan</p>
-                            <p className="personPosition">Front-End Developer</p>
-                            <p className="personText">Lorem Ipsum has been the standard "fish" for Latin texts since the early
+                    <li className={s.teamItem}>
+                        <img src={imageOleksii} alt="Olexii" className={s.personPhoto} />
+                        <div className={s.aboutPerson}>
+                            <p className={s.personName}>Olexii Fedorkan</p>
+                            <p className={s.personPosition}>Front-End Developer</p>
+                            <p className={s.personText}>Lorem Ipsum has been the standard "fish" for Latin texts since the early
                             16th
                     century.</p>
                         </div>
                     </li>
 
-                    <li class="teamItem">
-                        <img src="./img/ekaterina.jpg" alt="Ekaterina" class="personPhoto" />
-                        <div className="aboutPerson">
-                            <p className="personName">Ekaterina Kruichkovskaya</p>
-                            <p className="personPosition">Front-End Developer</p>
-                            <p className="personText">Lorem Ipsum has been the standard "fish" for Latin texts since the early
+                    <li className={s.teamItem}>
+                        <img src={imageEkaterina} alt="Ekaterina" className={s.personPhoto} />
+                        <div className={s.aboutPerson}>
+                            <p className={s.personName}>Ekaterina Kruichkovskaya</p>
+                            <p className={s.personPosition}>Front-End Developer</p>
+                            <p className={s.personText}>Lorem Ipsum has been the standard "fish" for Latin texts since the early
                             16th
                     century.</p>
                         </div>
                     </li>
-                    <li className="teamItem">
-                        <img src="./img/ivan.jpg" alt="Ivan" className="personPhoto" />
-                        <div className="aboutPerson">
-                            <p className="personName">Ivan Vitenko</p>
-                            <p className="personPosition">Front-End Developer</p>
-                            <p className="personText">Lorem Ipsum has been the standard "fish" for Latin texts since the early
+                    <li className={s.teamItem}>
+                        <img src={imageIvan} alt="Ivan" className={s.personPhoto} />
+                        <div className={s.aboutPerson}>
+                            <p className={s.personName}>Ivan Vitenko</p>
+                            <p className={s.personPosition}>Front-End Developer</p>
+                            <p className={s.personText}>Lorem Ipsum has been the standard "fish" for Latin texts since the early
                             16th
                     century.</p>
                         </div>
                     </li>
-                    <li className="teamItem">
-                        <img src="./img/taisia.jpg" alt="Taisiia" class="personPhoto" />
-                        <div className="aboutPerson">
-                            <p className="personName">Taisiia Morhunska</p>
-                            <p className="personPosition">Front-End Developer</p>
-                            <p className="personText">Lorem Ipsum has been the standard "fish" for Latin texts since the early
+                    <li className={s.teamItem}>
+                        <img src={imageTasia} alt="Taisiia" className={s.personPhoto} />
+                        <div className={s.aboutPerson}>
+                            <p className={s.personName}>Taisiia Morhunska</p>
+                            <p className={s.personPosition}>Front-End Developer</p>
+                            <p className={s.personText}>Lorem Ipsum has been the standard "fish" for Latin texts since the early
                             16th
                     century.</p>
                         </div>
                     </li>
-                    <li className="teamItem">
-                        <img src="./img/anton.jpg" alt="Anton" class="personPhoto" />
-                        <div className="aboutPerson">
-                            <p className="personName">Anton Lazurko</p>
-                            <p className="personPosition">Front-End Developer</p>
-                            <p className="personText">Lorem Ipsum has been the standard "fish" for Latin texts since the early
+                    <li className={s.teamItem}>
+                        <img src={imageAnton} alt="Anton" className={s.personPhoto} />
+                        <div className={s.aboutPerson}>
+                            <p className={s.personName}>Anton Lazurko</p>
+                            <p className={s.personPosition}>Front-End Developer</p>
+                            <p className={s.personText}>Lorem Ipsum has been the standard "fish" for Latin texts since the early
                             16th
                     century.</p>
                         </div>
                     </li>
-                    <li class="teamItem">
-                        <img src={imageDasha} alt="Daria" class="personPhoto" />
-                        <div className="aboutPerson">
-                            <p className="personName">Dasha Voitekhovskaya</p>
-                            <p className="personPosition">Front-End Developer</p>
-                            <p className="personText">Lorem Ipsum has been the standard "fish" for Latin texts since the early
+                    <li className={s.teamItem}>
+                        <img src={imageDasha} alt="Daria" className={s.personPhoto} />
+                        <div className={s.aboutPerson}>
+                            <p className={s.personName}>Dasha Voitekhovskaya</p>
+                            <p className={s.personPosition}>Front-End Developer</p>
+                            <p className={s.personText}>Lorem Ipsum has been the standard "fish" for Latin texts since the early
                             16th
                     century.</p>
                         </div>
                     </li>
-                    <li class="teamItem">
-                        <img src="./img/fedir.jpg" alt="Fedir" className="personPhoto" />
-                        <div className="aboutPerson">
-                            <p className="personName">Fedir Havrylyak</p>
-                            <p className="personPosition">Front-End Developer</p>
-                            <p className="personText">Lorem Ipsum has been the standard "fish" for Latin texts since the early
+                    <li className={s.teamItem}>
+                        <img src={imageFedir} alt="Fedir" className={s.personPhoto} />
+                        <div className={s.aboutPerson}>
+                            <p className={s.personName}>Fedir Havrylyak</p>
+                            <p className={s.personPosition}>Front-End Developer</p>
+                            <p className={s.personText}>Lorem Ipsum has been the standard "fish" for Latin texts since the early
                             16th
                     century.</p>
                         </div>
                     </li>
-                    <li className="teamItem">
-                        <img src="./img/oleksandr.jpg" alt="Oleksandr" className="personPhoto" />
-                        <div className="aboutPerson">
-                            <p className="personName">Oleksandr Bezvuglyak</p>
-                            <p className="personPosition">Front-End Developer</p>
-                            <p className="personText">Lorem Ipsum has been the standard "fish" for Latin texts since the early
+                    <li className={s.teamItem}>
+                        <img src={imageOleksandr} alt="Oleksandr" className={s.personPhoto} />
+                        <div className={s.aboutPerson}>
+                            <p className={s.personName}>Oleksandr Bezvuglyak</p>
+                            <p className={s.personPosition}>Front-End Developer</p>
+                            <p className={s.personText}>Lorem Ipsum has been the standard "fish" for Latin texts since the early
                             16th
                     century.</p>
                         </div>
