672a0c91be0896546ea3cb347ffd6ee2db5e8b87
unknown <63597446+antonlazurko@users.noreply.github.com>
2021-04-14 13:10:01 +0300

    realized scroll to hash in Card; change team json;

--- src/views/ContactsPage/ContactCard/ContactCard.js
+++ src/views/ContactsPage/ContactCard/ContactCard.js
@@ -1,14 +1,21 @@
-import { Suspense } from 'react';
+import { Suspense, useRef, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import s from './ContactCard.module.css';
 import defaultImg from '../../../img/default-user.jpg';
 
 const ContactCard = ({ members }) => {
+  const refToScroll = useRef();
+  const scrollToHashRef = () =>
+    refToScroll.current?.scrollIntoView({ behavior: 'smooth' });
+
   const { memberId } = useParams();
   const { imgUrl, title, name, descr } = members.find(
     member => member.id === Number.parseInt(memberId),
   );
+  useEffect(() => {
+    setTimeout(scrollToHashRef, 200);
+  });
   return (
     <div className={`${s.onePersonContainer} ${s.container}`}>
       <hr className={s.personLine} />
@@ -25,10 +32,12 @@ const ContactCard = ({ members }) => {
           />
         </Suspense>
 
-        <div className={s.aboutOnePerson}>
+        <div className={s.aboutOnePerson} ref={refToScroll}>
           <p className={s.onePersonName}>{name}</p>
           <p className={s.onePersonPosition}>{title}</p>
-          <p className={s.onePersonText}>{descr}</p>
+          <p className={s.onePersonText} id="descr">
+            {descr}
+          </p>
         </div>
       </div>
     </div>
