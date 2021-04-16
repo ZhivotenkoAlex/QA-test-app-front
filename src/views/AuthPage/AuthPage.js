923dc95311f0ae56aa613e8ee3239ed51b81dc1b
unknown <63597446+antonlazurko@users.noreply.github.com>
2021-04-16 11:41:57 +0300

    added spinner on login and register pending

--- src/views/AuthPage/AuthPage.js
+++ src/views/AuthPage/AuthPage.js
@@ -1,14 +1,22 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import {
   logIn,
   register,
 } from '../../redux/authorization/authorization-operations';
+import {
+  getLogInPending,
+  getRegisterPending,
+} from '../../redux/authorization/authorization-selectors';
 
 import styles from './AuthPage.module.css';
 import AuthForm from '../../components/AuthForm';
+import Loader from '../../components/Loader/Loader';
+
 const AuthPage = () => {
   const dispatch = useDispatch();
+  const logInPending = useSelector(getLogInPending);
+  const registerPending = useSelector(getRegisterPending);
 
   // submit function
   const handleSubmint = async (credentials, targetName) => {
@@ -33,25 +41,24 @@ const AuthPage = () => {
           <h1 className={styles.homeLoginTitle}>Pro Test</h1>
           <p className={styles.homeText}>
             <span className={styles.homeAccent}>[</span> We will help you find
-            weak points <br/> in knowledge so that you can strengthen it. We will show
-            you what is relevant to know for a
+            weak points <br /> in knowledge so that you can strengthen it. We
+            will show you what is relevant to know for a
             <span className={styles.homeAccent}> QA Engineer</span> and will try
             to make the learning process more diverse_
             <span className={styles.homeAccent}>]</span>
           </p>
         </div>
         <div className={styles.formRegister}>
+          {logInPending && <Loader />} {registerPending && <Loader />}
           <p className={styles.authorizeTitle}>
             You can use your Google Account to authorize:
           </p>
-
           <a
             className={styles.buttonGoogleLink}
             href="https://safe-bayou-94848.herokuapp.com/api/auth/google"
           >
             <button className={styles.buttonGoogle}></button>
           </a>
-
           <p className={styles.authorizeTitle}>
             Or login to our app using e-mail and password:
           </p>
